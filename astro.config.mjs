194affd1d4f031b3d74ab8f36e4d74c0b05ea983
Ahmet <ayavuz0524@gmail.com>
2025-10-06 11:46:26 +0300

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
-  site: 'kapi-ureticileri-q0437kbcw-ahmet-yavuzs-projects-1c10fc7c.vercel.app',
+  site: 'kkapi-ureticileri.vercel.app',
   integrations: [sitemap()],
   build: {
     assets: 'assets'
