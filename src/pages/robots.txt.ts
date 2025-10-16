aa7c2ba451092daa877b3ef5a796a8ab1b988931
Ahmet <ayavuz0524@gmail.com>
2025-10-16 17:02:33 +0300

    9

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -6,7 +6,7 @@ Sitemap: https://kapi-ureticileri.vercel.app/sitemap-index.xml`;
   return new Response(body, {
     headers: {
       'Content-Type': 'text/plain; charset=utf-8',
-      'Cache-Control': 'no-cache, no-store, must-revalidate'
+      'Cache-Control': 'public, max-age=3600, s-maxage=3600'
     }
   });
 }
